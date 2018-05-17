b7f21d682fdd0067f4874a4dbaa9dbb9e6ce0715
Ivan Olivares <ivan.olivares@globant.com>
2018-05-17 18:51:37 -0400

    Creando contrato incial de flightStatus

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Responds a dummy
- *
- * @param {!Object} req Cloud Function request context.
- * @param {!Object} res Cloud Function response context.
- */
-exports.flightStatus = (req, res) => {
-  res.send('Awesome Flight Status!')
-}
---
+++ service/index.js
@@ -0,0 +1,38 @@
+const fs = require('./flightStatus')
+const w = require('./weather')
+
+/**
+ * Responds a flight status with weather data.
+ *
+ * @param {!Object} req Cloud Function request context.
+ * @param {!Object} res Cloud Function response context.
+ */
+exports.flightStatus = (req, res) => {
+  res.send(200, {
+    flight: 'LA2012',
+    date: '2018-05-18',
+    origin: {
+      city: 'Santiago',
+      fullAirport: 'Arturo Merino Benitez',
+      code: 'SCL'
+    },
+    destination: {
+      city: 'Antofagasta',
+      fullAirport: 'Andres Sabella',
+      code: 'ANF'
+    },
+    status: 'On-Time',
+    weather: {
+      origin: {
+        description: 'Cloudy sky',
+        temperature: '23',
+        icon: '04n'
+      },
+      destination: {
+        description: 'Cloudy sky',
+        temperature: '24',
+        icon: '04n'
+      }
+    }
+  })
+}
